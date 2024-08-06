ef60e92572f9f0cfb51e6d0ada43e9140ed4e06b
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-08-06 16:40:22 +0800

    fix: order

--- src/rawApps.ts
+++ src/rawApps.ts
@@ -1,7 +1,7 @@
 import { RawApp, RawAppGroup } from '@gkd-kit/api';
 import categories from './categories';
 import { batchImportApps } from '@gkd-kit/tools';
-import { orderList } from './globalGroups';
+import { OPEN_AD_ORDER, YOUTH_MODE_ORDER, UPDATE_PROMPT_ORDER } from './globalGroups';
 
 const apps = await batchImportApps(`${import.meta.dirname}/apps`);
 const rawApps: RawApp[] = [];
@@ -11,13 +11,13 @@ apps.forEach((appConfig) => {
     for (let i of categories) {
       if (g.name.startsWith(i.name)) {
         if (!g.name.startsWith(categories[0].name)) g.enable = false;
-        else g.order = orderList[categories[0].key];
+        else g.order = OPEN_AD_ORDER;
 
         if (g.name.startsWith(categories[1].name))
-          g.order = orderList[categories[1].key];
+          g.order = YOUTH_MODE_ORDER;
 
         if (g.name.startsWith(categories[2].name))
-          g.order = orderList[categories[2].key];
+          g.order = UPDATE_PROMPT_ORDER;
 
         return;
       }
