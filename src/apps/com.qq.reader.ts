1c677cb61e638cb5c61227356a19a71f247d42b7
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-08-07 17:12:17 +0800

    perf: QQ阅读

--- src/apps/com.qq.reader.ts
+++ src/apps/com.qq.reader.ts
@@ -15,28 +15,57 @@ export default defineGkdApp({
         {
           key: 0,
           matches:
-            'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView <<n [id="android:id/content"]',
+            'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true][visibleToUser=true] + TextView[text=null] <<n [id="android:id/content"]',
           snapshotUrls: 'https://i.gkd.li/i/14978239',
         },
       ],
     },
     {
-      key: 1,
+      key: 2,
+      name: '青少年模式',
+      fastQuery: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id="com.qq.reader:id/tv_i_know"]',
+      snapshotUrls: 'https://i.gkd.li/i/13194867',
+    },
+    {
+      key: 3,
       name: '局部广告-右下角悬浮卡片广告',
       desc: '点击关闭',
       rules: [
         {
           fastQuery: true,
-          activityIds: 'com.qq.reader.activity.MainActivity',
-          matches: '[id="com.qq.reader:id/operating_activity_close"]',
-          exampleUrls:
-            'https://m.gkd.li/57941037/17b6444c-e275-4ee4-b1d7-bd7369e08e55',
-          snapshotUrls: 'https://i.gkd.li/i/14140100',
+          activityIds: [
+            'com.qq.reader.activity.MainActivity',
+            'com.qq.reader.activity.MainFlutterActivity',
+          ],
+          matches: '[vid="operating_activity_close_view"]',
+          exampleUrls: 'https://e.gkd.li/29b1cea7-6984-4d51-b1a0-f5ffd1b0d727',
+          snapshotUrls: [
+            'https://i.gkd.li/i/14140100',
+            'https://i.gkd.li/i/16517211',
+          ],
         },
       ],
     },
     {
-      key: 2,
+      key: 4,
+      name: '更新提示-内测邀请弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      fastQuery: true,
+      rules: [
+        {
+          matches: '[vid="upgrade_dialog_close_btn"][clickable=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/03e35d57-5f40-4ccb-911a-5f9061eab46e',
+          snapshotUrls: 'https://i.gkd.li/i/14376088',
+        },
+      ],
+    },
+    {
+      key: 5,
       name: '分段广告-阅读页面广告',
       desc: '点击关闭-点击[关闭本条广告]',
       fastQuery: true,
@@ -56,8 +85,7 @@ export default defineGkdApp({
             'https://m.gkd.li/57941037/eb2bca3c-abbc-4666-b505-cdaeeec6a839',
           snapshotUrls: 'https://i.gkd.li/i/14767126',
         },
-
-        // 预留 key
+        // 预留key
         {
           preKeys: [0, 1],
           key: 50,
